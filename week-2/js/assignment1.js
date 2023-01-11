a25e1543da3e188f4206b41ff21b61a2a2591d12
FloraYin <flora8854516@gmail.com>
2023-01-11 20:27:28 +0800

    Add assignment 1

--- week-2/js/assignment1.js
+++ week-2/js/assignment1.js
@@ -1,14 +1,12 @@
 /* Function and Array
  Complete the function below to find the max number of the passing array of numbers.
- Reminder: you can't use built-in Math.max() or sort() to complete this assignment. */
- // a b c d
- // 
+ Reminder: you can't use built-in Math.max() or sort() to complete this assignment. */ 
 
  function max(numbers) {
     // your code here
-    let maxNumber = "";
+    let maxNumber = 0;
     for (let i = 0; i <= numbers.length; i++) {
-        if (maxNumber >= numbers[i]) {
+        if (maxNumber <= numbers[i]) {
             maxNumber = numbers[i];
         }
     }
