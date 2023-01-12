8d65e1455c6dd3632ba44d4a72f55c19d06bed8f
FloraYin <flora8854516@gmail.com>
2023-01-12 22:12:15 +0800

    Add Script to switch between show/close

--- week-2/js/script.js
+++ week-2/js/script.js
@@ -7,6 +7,20 @@ bannerUpdate.addEventListener( 'click', () => {
     title.textContent = 'Have a Good Time!';
 });
 
+//Request 2: Click to Show/Close Menu.
+
+const menuBtn = document.querySelector('.fa-bars');
+const showMenu = document.querySelector('.nav-container');
+const cancelBtn = document.querySelector('.cancel-btn');
+
+menuBtn.addEventListener( 'click', () => {
+    showMenu.style.display = 'inline-block';
+});
+
+cancelBtn.addEventListener( 'click', () => {
+    showMenu.style.display = 'none';
+});
+
 
 //Request 3: Click to Show More Content Boxes.
 
@@ -16,3 +30,4 @@ button.addEventListener( 'click', () => {
     const contentBoxesToShow = document.querySelector('.row2');
     contentBoxesToShow.style.display = 'flex';
 });
+
