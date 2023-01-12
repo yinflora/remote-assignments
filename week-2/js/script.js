52b927152212109c590b852159b540ae51bd972e
FloraYin <flora8854516@gmail.com>
2023-01-12 21:11:05 +0800

    Click to Show More Content Boxes

--- week-2/js/script.js
+++ week-2/js/script.js
@@ -1,6 +1,18 @@
+//Request 1: Click to Change Text.
+
 const bannerUpdate = document.querySelector('.banner');
 
 bannerUpdate.addEventListener( 'click', () => {
     const title = document.querySelector('h1');
     title.textContent = 'Have a Good Time!';
+});
+
+
+//Request 3: Click to Show More Content Boxes.
+
+const button = document.querySelector('.btn');
+
+button.addEventListener( 'click', () => {
+    const contentBoxesToShow = document.querySelector('.row2');
+    contentBoxesToShow.style.display = 'flex';
 });
