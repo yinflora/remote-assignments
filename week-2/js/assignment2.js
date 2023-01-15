8981913227da8f49f8c952c83e65cbc18fd30d90
FloraYin <flora8854516@gmail.com>
2023-01-15 13:24:47 +0800

    Modify to print the correct message

--- week-2/js/assignment2.js
+++ week-2/js/assignment2.js
@@ -3,9 +3,14 @@
 
 function calculate(args) {
     // your code here
-    const answer = args.n1 + args.op + args.n2;
-    return eval(answer);
+    // const answer = args.n1 + args.op + args.n2;
+    // return eval(answer);
+    if (args.op === "+" || args.op === "-") {
+        return eval(args.n1 + args.op + args.n2);
+    } else {
+        return "Not supported";
+    }
 }
-console.log(calculate({ n1: 2, n2: 3, op: '+' })); // expected output: 5
-console.log(calculate({ n1: 5, n2: 2, op: '-' })); // expected output: 3
-console.log(calculate({ n1: 1, n2: 6, op: 'x' })); // expected output: 'Not supported'
+console.log(calculate({ n1: 2, n2: 3, op: "+" })); // expected output: 5
+console.log(calculate({ n1: 5, n2: 2, op: "-" })); // expected output: 3
+console.log(calculate({ n1: 1, n2: 6, op: "x" })); // expected output: 'Not supported'
