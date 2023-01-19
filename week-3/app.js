5ee5a621b8a566e1730614fde5d31bcfb1e4f56a
FloraYin <flora8854516@gmail.com>
2023-01-19 14:51:24 +0800

    Show an HTML page when enter the server

--- week-3/app.js
+++ week-3/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-    res.send("Hello, My Server!");
+    res.render("index");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("The application is running on localhost:3000!");
+});
