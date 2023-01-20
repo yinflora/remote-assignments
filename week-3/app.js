8442820bed9087f395bfd1d5d232f721eabb6fba
FloraYin <flora8854516@gmail.com>
2023-01-20 21:47:11 +0800

    Assignment 3: Connect to Backend API by AJAX

--- week-3/app.js
+++ week-3/app.js
@@ -5,13 +5,13 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(express.static("public"));
 
 const routes = require("./routes");
 
 app.use(routes);
 
 app.listen(3000, () => {
-    console.log("The application is running on localhost:3000!");
+  console.log("The application is running on localhost:3000!");
 });
--- week-3/routes/index.js
+++ week-3/routes/index.js
@@ -2,24 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index");
+  res.send("Hello, My Server!");
 });
 
 router.get("/getData", (req, res) => {
-    let answer;
-    if (!req.query.number) {
-        res.send("Lack of Parameter");
+  let answer;
+  if (!req.query.number) {
+    res.send("Lack of Parameter");
+  } else {
+    if (isNaN(req.query.number)) {
+      answer = "Wrong Parameter";
     } else {
-        if (isNaN(req.query.number)) {
-            answer = "Wrong Parameter";
-        } else {
-            answer =
-                ((1 + Number(req.query.number)) * Number(req.query.number)) / 2;
-        }
-        res.send(`${answer}`);
+      answer = ((1 + Number(req.query.number)) * Number(req.query.number)) / 2;
     }
+    res.send(`${answer}`);
+  }
+});
 
-    // res.render("getData", answer);
+router.post("/getData", (req, res) => {
+  let answer = ((1 + Number(req.body.number)) * Number(req.body.number)) / 2;
+  res.send(`${answer}`);
 });
 
 module.exports = router;
