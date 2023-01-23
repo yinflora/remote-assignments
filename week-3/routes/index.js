5f5959ebfb3b036ed9ef8cfdc02f0e424d040824
FloraYin <flora8854516@gmail.com>
2023-01-23 18:41:24 +0800

    Add assignment 5

--- week-3/routes/index.js
+++ week-3/routes/index.js
@@ -1,8 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
+/* --- Assignment 1 --- */
+
 router.get("/", (req, res) => {
   res.send("Hello, My Server!");
 });
+
+
+/* --- Assignment 5 --- */
+
+router.get("/myName", (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render("myName", { name });
+  } else {
+    res.redirect("/trackName");
+  }
+});
+
+router.get("/trackName", (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect("/myName");
+  } else {
+    res.render("trackName");
+  }
+});
+
+router.post("/trackName", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/myName");
+});
+
+router.get("/trackName", (req, res) => {
+  res.cookie("username", req.query.name);
+  const name = res.cookies.username;
+  res.redirect("/myName");
+});
+
 
 module.exports = router;
