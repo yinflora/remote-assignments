847f213f73e036894ed852a376af14a16817d021
FloraYin <flora8854516@gmail.com>
2023-01-29 22:05:04 +0800

    Add Function for rendering the response into index.html

--- week-4/Assignment2/script.js
+++ week-4/Assignment2/script.js
@@ -4,16 +4,31 @@ Complete the functions below to make an AJAX call to a URL by Fetch API, and sho
 response data in the page. You may render UI with any style. 
 ----------------------------------------- */
 
+// Function for getting response from API
 function ajax(url) {
-    fetch("https://appworks-school.github.io/Remote-Assignment-Data/products")
-        .then( res => res.json() ) // Parse response to JSON
-        .then( result => console.log(result) )
+    return fetch(url).then( res => res.json() ) // Parse response to JSON
 }
+// Function for rendering the response into index.html
 function render(data) {
-    
+    const productSection = document.querySelector(".list");
+
+    data.forEach( product => {
+        let name = product.name;
+        let price = product.price;
+        let description = product.description;
+
+        const productElement = document.createElement("div");
+        productElement.innerHTML = `
+            <p>${name}</p>
+            <p>${price}</p>
+            <p>${description}</p>
+        `;
+
+        productSection.append(productElement);
+    });
 }
 const url = "https://appworks-school.github.io/Remote-Assignment-Data/products";
 ajax(url).then((data) => {
-render(data);
+    render(data);
 });
 // you should get product information in JSON format and render data in the page
