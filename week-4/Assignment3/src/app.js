65d4e6aca171429f7fc1e80d3875d919c8e70d62
FloraYin <flora8854516@gmail.com>
2023-01-31 17:59:29 +0800

    Click to Show/Close Menu.

--- week-4/Assignment3/src/app.js
+++ week-4/Assignment3/src/app.js
@@ -1,9 +1,102 @@
 // ./src/app.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const root = document.getElementById('root');
-const App = () => <h1>React</h1>;
+function App() {
+    return (
+        <div>
+            <Header />
 
-ReactDOM.render(<App />, root);
+            <Banner />
+
+            <ContentBox />
+        </div>
+    );
+};
+
+function Header() {
+    return (
+        <header className="top-nav">
+            <div className="main-header">
+                <p className="name"><a href="#">Website Title / Logo</a></p>
+                <i className="fa-solid fa-bars"></i>
+                <div className="nav-container">
+                    <ul className="main-nav">
+                        <li><a href="#">Item1</a></li>
+                        <li><a href="#">Item2</a></li>
+                        <li><a href="#">Item3</a></li>
+                        <li><a href="#">Item4</a></li>
+                    </ul>
+                    <button className="cancel-btn">X</button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+function Banner() {
+    // Track the message state with useSate
+    const [ message, setMessage ] = useState("Welcome Message");
+
+    return (
+        // JSX expressions must have one parent element in React
+        <> 
+            <div className="banner">
+                {/* Update the message state */}
+                <h1 className="headline" onClick={() => setMessage("Have a Good Time!")}>{ message }</h1>
+            </div>
+        </>
+    );
+};
+
+function ContentBox() {
+    return (
+        <> 
+            <div className="title">
+                <h2>Section Title</h2>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <p>Content Box 1</p>
+                </div>
+                <div className="col">
+                    <p>Content Box 2</p>
+                </div>
+                <div className="col">
+                    <p>Content Box 3</p>
+                </div>
+                <div className="col">
+                    <p>Content Box 4</p>
+                </div>
+            </div>
+
+            <div className="call-to-action">
+                <button className="btn">Call to Action</button>
+             </div>
+
+            <div className="row2">
+                <div className="col">
+                    <p>Content Box 5</p>
+                </div>
+                <div className="col">
+                    <p>Content Box 6</p>
+                </div>
+                <div className="col">
+                    <p>Content Box 7</p>
+                </div>
+                <div className="col">
+                    <p>Content Box 8</p>
+                </div>
+            </div>
+        </>
+    );
+};
+
+
+// 取得在 HTML 中事先定義好的容器元素，以作為之後 React 產生 DOM elements 結果的輸出容器
+const container = document.getElementById('root');
+// 用這個容器元素來建立一個 React App 的畫面渲染管轄入口 (root)
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
