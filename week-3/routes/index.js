cc01d388e049ede4c6f6cca4bce169df7f62bb2f
FloraYin <flora8854516@gmail.com>
2023-02-03 12:21:59 +0800

    Merge router.post(/myName) and router.get(/trackName)

--- week-3/routes/index.js
+++ week-3/routes/index.js
@@ -1,33 +1,41 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 /* --- Assignment 1 --- */
 
-router.get("/", (req, res) => {
-  res.send("Hello, My Server!");
+router.get('/', (req, res) => {
+    res.send('Hello, My Server!');
 });
-
 
 /* --- Assignment 5 --- */
 
-router.get("/myName", (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.render("myName", { name });
-  } else {
-    res.render("myName");
-  }
+router.get('/myName', (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.render('myName', { name });
+    } else {
+        res.render('myName');
+    }
 });
 
-router.post("/myName", (req, res) => {
-  const name = req.body.username;
-  res.cookie("username", name);
-  res.redirect("/trackName?name=" + name);
-});
+// router.post("/myName", (req, res) => {
+//   res.cookie('username', req.body.username);
+//   res.redirect("/trackName?name=" + name);
+// });
 
-router.get("/trackName", (req, res) => {
-  res.cookie('username', req.query.name);
-  res.redirect("/myName");
+// router.get("/trackName", (req, res) => {
+//   res.cookie('username', req.query.name);
+//   res.redirect("/myName");
+// });
+
+router.post('/trackName', (req, res) => {
+    if (req.body.username) {
+        res.cookie('username', req.body.username);
+        res.redirect('/myName');
+    } else if (req.query.name) {
+        res.cookie('username', req.query.name);
+        res.redirect('/myName');
+    }
 });
 
 module.exports = router;
