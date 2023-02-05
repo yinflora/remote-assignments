0375c62a95ec56a6fbd3fa3a98e2dcb3d8550116
FloraYin <flora8854516@gmail.com>
2023-02-05 10:26:00 +0800

    Add get request to /trackName

--- week-3/routes/index.js
+++ week-3/routes/index.js
@@ -18,21 +18,15 @@ router.get('/myName', (req, res) => {
     }
 });
 
-// router.post("/myName", (req, res) => {
-//   res.cookie('username', req.body.username);
-//   res.redirect("/trackName?name=" + name);
-// });
-
-// router.get("/trackName", (req, res) => {
-//   res.cookie('username', req.query.name);
-//   res.redirect("/myName");
-// });
-
 router.post('/trackName', (req, res) => {
     if (req.body.username) {
         res.cookie('username', req.body.username);
         res.redirect('/myName');
-    } else if (req.query.name) {
+    }
+});
+
+router.get('/trackName', (req, res) => {
+    if (req.query.name) {
         res.cookie('username', req.query.name);
         res.redirect('/myName');
     }
