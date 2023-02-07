00878d33bc8788a3d98ee8c5117544c5806d424a
FloraYin <flora8854516@gmail.com>
2023-02-07 13:07:59 +0800

    Add useEffect to fetch the new data

--- week-4/Assignment4/src/App.js
+++ week-4/Assignment4/src/App.js
@@ -2,26 +2,22 @@ import { useState, useEffect } from 'react';
 
 const App = () => {
     const [data, setData] = useState([]);
-    // const [page, setPage] = useState(1);
+    const [page, setPage] = useState(1);
+
+    const api = `https://api.github.com/orgs/facebook/repos?per_page=5&amp;page=${page}`;
 
     useEffect(() => {
-        fetch(
-            // `https://api.github.com/orgs/facebook/repos?per_page=5&amp;page=${page}`
-            'https://api.github.com/orgs/facebook/repos?per_page=5&amp;page=1'
-        )
+        fetch(api)
             .then((res) => res.json())
-            .then((data) => setData(data));
-    }, []);
-
-    // function togglePage() {
-    //   setPage(page++);
-    // }
+            .then((newData) => setData(data.concat(newData)));
+    }, [api, page]);
 
     return (
         <div className="App">
-            <Items itemData={data} />
-            {/* <button onClick={togglePage}>More</button> */}
-            <button>More</button>
+            <div className="box">
+                <Items itemData={data} />
+            </div>
+            <button onClick={() => setPage(page + 1)}>More</button>
         </div>
     );
 };
@@ -36,15 +32,13 @@ const Items = ({ itemData }) => {
             <p className="description">{item.description}</p>
             <div className="topic">
                 {item.topics.map((topic) => (
-                    <span className="topic-item" key={topic}>
-                        {topic}
-                    </span>
+                    <span key={topic}>{topic}</span>
                 ))}
             </div>
         </div>
     ));
 
-    return <div className="box">{itemList}</div>;
+    return <>{itemList}</>;
 };
 
 export default App;
